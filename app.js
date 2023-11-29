6c0ecb8479bb40627517c9e9d4c9b4e294566707
PavaniMidathana <121936768+PavaniMidathana@users.noreply.github.com>
2023-11-29 10:23:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+
 const cors = require("cors");
 
 const app = express();
